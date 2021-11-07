60ca2008a0dd5acfb0854f499fb6edbc9888af64
S1gB1n <58967059+S1gB1n@users.noreply.github.com>
2021-11-07 10:33:23 -0700

    Add files via upload

--- js/ch08-proj01.js
+++ js/ch08-proj01.js
@@ -1,6 +1,27 @@
 
-const tax_rate = prompt('Enter tax rate (0.10)');
-const shipping_threshold = prompt('Enter shipping threshold (1000)');
+const tax_rate = parseFloat( prompt('Enter tax rate (0.10)') );
+const shipping_threshold = parseFloat( prompt('Enter shipping threshold (1000)') );
 
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
+
+document.write("<body>" +
+               "<table class='table-fill'>");
+
+document.write("<thead>" +
+               "<tr>" + 
+               "<th colspan='2'>Product</th>" +
+               "<th>#</th>" +
+               "<th>Price</th>" +
+               "<th>Amount</th>" +
+               "</tr>" + 
+               "</thead>");
+
+document.write("<tbody>");
+for(let i = 0; i < cart.length; i++){
+   outputCartRow(cart[i].product, cart[i].quantity);   
+}
+outputCalculation(cart, tax_rate, shipping_threshold);
+document.write("</tbody>");
+document.write("</table>" + 
+               "</body>");
